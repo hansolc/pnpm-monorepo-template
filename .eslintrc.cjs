cd8267ffe9faae48bcfb36dd6bb4a777a6ff3087
hansolc <hsc080808@naver.com>
2025-02-24 22:50:52 +0900

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
+/**
+ * root: 현재 디렉토리를 ESLint 설정의 최상위(root) 경로로 지정.
+ * - 하위 프로젝트들이 .eslintrc를 추가로 정의하더라도, 이 설정을 기준으로 상속받도록 강제.
+ * 
+ */
+
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -20,7 +22,7 @@ module.exports = {
       },
     },
     {
-      files: ['*.d.ts'], // .d.ts 파일에만 적용
+      files: ['**/*.d.ts'], // .d.ts 파일에만 적용
       rules: {
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-empty-object-type': 'off',
